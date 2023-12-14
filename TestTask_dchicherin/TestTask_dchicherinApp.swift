25d447fbf3d3ceceeefea673906319920fe6e071
Dmitrii <navssane@gmail.com>
2023-12-14 20:34:32 +0300

    Initial Commit

---
+++ TestTask_dchicherin/TestTask_dchicherinApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTask_dchicherinApp.swift
+//  TestTask_dchicherin
+//
+//  Created by Dmitry Chicherin on 14/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct TestTask_dchicherinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
